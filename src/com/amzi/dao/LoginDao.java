0cfa458412eb6b1b8d0edf320e4af627c5e08893
Victor Fernandes <victorvalenca@gmail.com>
2017-02-03 21:09:18 -0500

    LoginDao: Add sign-up function

--- src/com/amzi/dao/LoginDao.java
+++ src/com/amzi/dao/LoginDao.java
@@ -12,7 +12,7 @@ public class LoginDao {
 	
     public static boolean validate(String name, String pass) {        
         boolean status = false;
-        Connection conn = null;
+        conn = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
 
@@ -20,7 +20,7 @@ public class LoginDao {
             db = new DatabaseDao();
             conn = db.getConnection();
             
-            
+            // Check password with SHA()
             pst = conn.prepareStatement("select * from Account where Username=? and Password=SHA(?)");
             pst.setString(1, name);
             pst.setString(2, pass);
@@ -50,5 +50,52 @@ public class LoginDao {
             
         }
         return status;
+    }
+    
+    public static boolean signUp(String username, String email, String password){
+    	conn = null;
+    	PreparedStatement pst = null;
+    	int rowCount = 0;
+    	
+    	try {
+    		db = new DatabaseDao();
+            conn = db.getConnection();
+            
+            // This checks if the client is attempting to sign up with a
+ 			// username or email already in use
+ 			PreparedStatement usrCheck = conn.prepareStatement("SELECT id FROM Account WHERE Username=? OR email=?");
+ 			usrCheck.setString(1, username);
+ 			usrCheck.setString(2, email);
+
+ 			if (!usrCheck.executeQuery().next()) // If insert is skipped, the
+ 													// row count is 0 and
+ 													// function returns false
+ 			{
+ 				pst = conn.prepareStatement("INSERT INTO Account (Username, AccountType, Email, Password)"
+ 						+ "VALUES"
+ 						+ "(?, 0, ?, SHA(?)"); 	// SHA() returns a string hash, mathematically one-way. There is no way
+ 												// to know what the password is, because it is not stored in the SQL
+ 												// server in plain text
+ 				pst.setString(0, username);
+ 				pst.setString(1, email);
+ 				pst.setString(2, password);
+ 				
+ 				rowCount = pst.executeUpdate(); // Catch how many rows were changed from the INSERT statement (Should be 1)
+ 			}
+ 			
+    	} catch (Exception e) {
+    		System.err.println(e);
+    	} finally {
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            db.closeConnection();
+            
+        }
+    	return (rowCount > 0);
     }
 }
